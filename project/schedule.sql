c68ad43735675ae26b84849134c37b4f0d833745
danny3978 <ekadl025@gmail.com>
2024-08-09 19:58:32 +0900

    Delete project directory

--- project/schedule.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user (
-         id INT PRIMARY KEY AUTO_INCREMENT,
-         name varchar(255) not null,
-         password varchar(100) not null,
-         something_to_do TEXT,
-         creation_date date not null,
-         revision_date date not null
-);
